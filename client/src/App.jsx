47460bcadc5edb95b4a243994409b01a243d858d
Muhammad Faizan Tariq <mfaizantariq99@gmail.com>
2024-06-07 14:35:56 +0500

    project setup

---
+++ client/src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <h1 className="text-3xl font-bold underline text-center">
+      Hello Expense Tracker App with GraphQL and Tailwind CSS!
+    </h1>
+  );
+}
