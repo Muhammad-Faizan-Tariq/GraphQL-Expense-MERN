1aed71f5c81c2122c792184fba3222b046a877b3
Muhammad Faizan Tariq <mfaizantariq99@gmail.com>
2024-06-12 21:57:42 +0500

    setup db & expressMiddleware

--- server/index.js
+++ server/index.js
@@ -1,12 +1,31 @@
+import express from "express"
+import http from "http"
+import cors from "cors"
+import dotevn from "dotenv"
 import { ApolloServer } from "@apollo/server";
-import { startStandaloneServer } from "@apollo/server/standalone";
 import mergedResolvers from "./resolvers/index.js";
 import mergedTypeDefs from "./typeDefs/index.js";
+import { expressMiddleware } from "@apollo/server/express4";
+import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
+import { db } from "./config/db.js";
+
+dotevn.config()
+const app = express()
+const httpServer = http.createServer(app)
+
 
 const server = new ApolloServer({
     typeDefs: mergedTypeDefs,
-    resolvers: mergedResolvers
+    resolvers: mergedResolvers,
+    plugins: [ApolloServerPluginDrainHttpServer({httpServer})]
 })
 
-const {url} = await startStandaloneServer(server)
-console.log(`GraphQL server is running at ${url}`);
+await server.start()
+// server.start()
+app.use("/", cors(), express.json(), expressMiddleware(server, {
+    context: async ({req}) => ({req}),
+}))
+
+await new Promise ((resolve)=> httpServer.listen({port: 4000}, resolve))
+await db();
+console.log(`Server is running on http://localhost:4000`);
